ee4c4a49ab937501acad0841dbdca7e112506bdc
hirushar2003 <hirushaperera651@gmail.com>
2024-10-11 17:29:21 +0530

    New - hero - all

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    document.getElementById('popup').style.display = 'block'; // Show the popup
+    document.getElementById('overlay').style.display = 'block'; // Show the overlay
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none'; // Hide the popup
+    document.getElementById('overlay').style.display = 'none'; // Hide the overlay
+}
